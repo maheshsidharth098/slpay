debf696c355d104c430a90486bd4ad7ab520d6e7
Haiyang Yu <yhyoscar@gmail.com>
2018-02-19 18:22:29 -0500

    updated

--- slpay.py
+++ slpay.py
@@ -1,12 +1,13 @@
 from scipy.optimize import linprog
 import numpy as np
 
-s  = [10000.0, 40000.0, 5000.0, 5000.0]
-r  = [0.055, 0.045, 0.05, 0.06]
+s  = [15732.46, 9178.09, 1530.07, 26485.93]
+r  = [0.079, 0.044, 0.0665, 0.0563]
+xmin = [0,    62.0, 36.0, 0]
 nyear  = 5
 K      = 1
-t0     = 201709
-pmax   = 1300.0
+t0     = 201803
+pmax   = 1500.0
 
 # Don't set pmax and nyear to very small values, otherwise there could be no solution
 #=================================================================================
@@ -25,7 +26,7 @@ ns = len(s)
 c   = np.zeros(ns*N) + K
 Aub = np.zeros([ns+N, ns*N])
 bub = [-i for i in s] + [pmax for i in range(N)]
-xb  = [ (0, pmax) for i in range(ns*N) ]
+xb  = [ (xmin[i], pmax) for i in range(ns*N) ]
 
 for i in range(ns):
     for j in range(N):
@@ -35,7 +36,7 @@ for i in range(ns):
         Aub[j+ns, ix]  = 1.0
 
 res = linprog(c, A_ub=Aub, b_ub=bub,  
-              bounds=xb, options=dict(bland=True, tol=1.0e-6, disp=True))
+              bounds=xb, options=dict(bland=True, tol=1.0e-3, disp=True))
 print 'Maximum monthly payment: ', pmax
 print '--------------------------------------'
 print ['YYYYMM'] + ['SL'+format(i+1) for i in range(ns)]
